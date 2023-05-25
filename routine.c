42729eeaeca98e81d91b66d67e77e6a86fe2d16d
emmameinert <110816436+emmameinert@users.noreply.github.com>
2023-05-25 18:14:52 +0300

    Add files via upload
    started working on routines

---
+++ routine.c
@@ -0,0 +1,29 @@
+#include "philo.h"
+
+void    eating()
+{
+    //thinking
+    //wait for eating
+    //grab forks
+    //eating
+    //wating
+    //lock
+    //get time
+    //update eating count
+    //unlock 
+    //sleeping
+}
+// void*  routine()
+// {
+//     int i;
+//     i = 0;
+//     while (i < 10000000)
+//     {
+//         pthread_mutex_lock(&mutex);
+//         eaten++;
+//         pthread_mutex_unlock(&mutex);
+//         i++;
+//     }
+//     return (eaten);
+// }  
+
