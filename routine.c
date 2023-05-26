351b8fedf4b7dd678dd6c4d03dd2905db3230c81
emmameinert <110816436+emmameinert@users.noreply.github.com>
2023-05-26 16:14:03 +0300

    Add files via upload
    well well

--- routine.c
+++ routine.c
@@ -1,29 +1,39 @@
 #include "philo.h"
 
-void    eating()
+void*  routine(void *philo)
 {
-    //thinking
-    //wait for eating
-    //grab forks
-    //eating
-    //wating
-    //lock
-    //get time
-    //update eating count
-    //unlock 
-    //sleeping
-}
-// void*  routine()
-// {
-//     int i;
-//     i = 0;
-//     while (i < 10000000)
-//     {
-//         pthread_mutex_lock(&mutex);
-//         eaten++;
-//         pthread_mutex_unlock(&mutex);
-//         i++;
-//     }
-//     return (eaten);
-// }  
+    int eaten;
+    t_philo *temp;
+    //long long time; 
+
+    temp = (t_philo *)philo;
+    eaten = 0;
+    temp[temp->id].start_time = get_time();
+    printf("flag: %d\n", temp->simu_flag); //finally works
+    printf("time : %lld\n", temp[temp->id].start_time);
+    // if (temp->simu_flag == 1)
+    // {
+    //     while (eaten < temp->data->not_each_philo_must_eat)
+    //     {
+    //         if ((temp->id % 2) == 0)
+    //         {
+    //             time  = time_diff(philo);//wait and think first
+    //             printf("[%lld] %s Philosopher starts thinking\n",time, GREEN);
+    //         }
+    //     eaten++;
+        //grab forks
+        //eating
+        //wating
+        //pthread_mutex_lock(&left_fork);
+        //pthread_mutex_lock(&rigth_fork);
+        //time_diff(temp->philo);
+        //eaten++;
+        //pthread_mutex_unlock(&left_fork);
+        //pthread_mutex_unlock(&rigth_fork);
+        //sleeping
+        
+    //     }
+    // }
+    return (philo);
+}  
 
