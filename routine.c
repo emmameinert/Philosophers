6753b3e618819c47f93a0e4ae6322c799b06ff03
emmameinert <110816436+emmameinert@users.noreply.github.com>
2023-05-29 15:25:08 +0300

    Add files via upload

--- routine.c
+++ routine.c
@@ -3,23 +3,31 @@
 void*  routine(void *philo)
 {
     int eaten;
-    t_philo *temp;
+    //t_philo *temp;
     //long long time; 
-
-    temp = (t_philo *)philo;
+    //temp = (t_philo *)philo;
+    printf("ROUTINE\n");
+    // printf("thread %d\n", temp->id);
+    // pthread_mutex_lock(&temp->data->left_fork);
+    // pthread_mutex_unlock(&temp->data->left_fork);
     eaten = 0;
-    temp[temp->id].start_time = get_time();
-    printf("flag: %d\n", temp->simu_flag); //finally works
-    printf("time : %lld\n", temp[temp->id].start_time);
-    // if (temp->simu_flag == 1)
-    // {
-    //     while (eaten < temp->data->not_each_philo_must_eat)
-    //     {
+    // pthread_mutex_lock(&temp->data->mutex);
+    // pthread_mutex_unlock(&temp->data->mutex);
     //         if ((temp->id % 2) == 0)
     //         {
     //             time  = time_diff(philo);//wait and think first
     //             printf("[%lld] %s Philosopher starts thinking\n",time, GREEN);
     //         }
+                //else
+                //eat
+    
+    // temp[temp->id].start_time = get_time();
+    // //printf("flag: %d\n", temp->simu_flag); //finally works
+    // printf("time : %lld\n", temp[temp->id].start_time);
+    // if (temp->simu_flag == 1)
+    // {
+    //     while (eaten < temp->data->not_each_philo_must_eat)
+    //     {
     //     eaten++;
         //grab forks
         //eating
@@ -34,6 +42,7 @@ void*  routine(void *philo)
         
     //     }
     // }
+    //join every thread 
     return (philo);
 }  
 
