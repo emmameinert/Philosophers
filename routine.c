1209859789e7df38a94f4a315f3360ff18a1b680
emmameinert <110816436+emmameinert@users.noreply.github.com>
2023-06-01 11:43:31 +0300

    Add files via upload

--- routine.c
+++ routine.c
@@ -1,48 +1,47 @@
 #include "philo.h"
 
-void*  routine(void *philo)
+void*    routine(void *philo)
 {
-    int eaten;
-    //t_philo *temp;
-    //long long time; 
-    //temp = (t_philo *)philo;
-    printf("ROUTINE\n");
-    // printf("thread %d\n", temp->id);
-    // pthread_mutex_lock(&temp->data->left_fork);
-    // pthread_mutex_unlock(&temp->data->left_fork);
-    eaten = 0;
-    // pthread_mutex_lock(&temp->data->mutex);
-    // pthread_mutex_unlock(&temp->data->mutex);
-    //         if ((temp->id % 2) == 0)
-    //         {
-    //             time  = time_diff(philo);//wait and think first
-    //             printf("[%lld] %s Philosopher starts thinking\n",time, GREEN);
-    //         }
-                //else
-                //eat
-    
-    // temp[temp->id].start_time = get_time();
-    // //printf("flag: %d\n", temp->simu_flag); //finally works
-    // printf("time : %lld\n", temp[temp->id].start_time);
-    // if (temp->simu_flag == 1)
-    // {
-    //     while (eaten < temp->data->not_each_philo_must_eat)
-    //     {
-    //     eaten++;
-        //grab forks
-        //eating
-        //wating
-        //pthread_mutex_lock(&left_fork);
-        //pthread_mutex_lock(&rigth_fork);
-        //time_diff(temp->philo);
-        //eaten++;
-        //pthread_mutex_unlock(&left_fork);
-        //pthread_mutex_unlock(&rigth_fork);
-        //sleeping
-        
-    //     }
-    // }
-    //join every thread 
-    return (philo);
-}  
+    t_philo *temp;
 
+    temp = (t_philo *)philo;
+    pthread_mutex_lock(&(temp->data->left_fork));
+    printf("hello\n");
+    pthread_mutex_unlock(&(temp->data->left_fork));
+    pthread_mutex_lock(&(temp->data->right_fork));
+    printf("%d\n", temp->id);
+    pthread_mutex_unlock(&(temp->data->right_fork));
+    return (NULL);
+}
+
+void    start_routine(t_philo **philo)
+{
+    int i;
+
+    i = 0;
+    printf("%lld\n", (philo[7])->data->philo_nb);
+    pthread_mutex_lock(&((*philo)->data->eating_lock)); //error
+    while (i < (*philo)->data->philo_nb)
+    {
+        philo[i]->id = i + 1;
+        printf("%d\n", philo[i]->id);
+        if (pthread_create(&(philo[i]->ph_thread), NULL, &routine, philo[i]) != 0) 
+        {
+            printf("Error with thread creation \n");
+            return ;
+        }
+        i++;   
+    }
+    printf("hierr: %d\n", i);
+    i = 0;
+    while (i < (*philo)->data->philo_nb)
+    {
+        if (pthread_join(philo[i]->ph_thread, NULL) != 0)
+        {
+            printf("mannn\n");
+            return ;
+        }
+        i++;
+    }
+    pthread_mutex_unlock(&((*philo)->data->eating_lock));
+}
