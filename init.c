407dbefb8937af91b8a29f9cacac942acd415cd2
emmameinert <110816436+emmameinert@users.noreply.github.com>
2023-06-01 11:43:01 +0300

    Add files via upload

--- init.c
+++ init.c
@@ -1,34 +1,45 @@
 #include "philo.h"
 
-t_philo *init_philo(char **argv)
+t_philo **init_philo(char **argv, t_data *data)
 {
-    t_philo *philo;
+    t_philo **philo;
     int philosophers;
+    int i;
 
+    i = 0;
     philosophers = ft_atol(argv[1]);
-    philo = malloc(sizeof(philo) * philosophers);
+    philo = malloc(sizeof(t_philo) * philosophers); 
     if (!philo)
         return (NULL);
+    while (i < philosophers)
+    {
+        philo[i] = malloc(sizeof(t_philo) * 1);
+        //check fehlt
+        philo[i]->data = data;
+        i++;
+    }
     return (philo);
 }
 
-t_data *init_data(char **argv)
+t_data *init_data(void)
+{
+    t_data *data;
+    
+    data = malloc(sizeof(t_data));
+    if (!data)
+        return (NULL);
+    return (data);
+}
+
+t_data *init_all(void)
 {
     t_data *data;
 
-    data = malloc(sizeof(t_data));
-	if (!data)
-		return (NULL);
+    data = init_data();
+    if (data == NULL)   
+        return (NULL);
+    pthread_mutex_init(&data->eating_lock, NULL);
     pthread_mutex_init(&data->left_fork, NULL);
     pthread_mutex_init(&data->right_fork, NULL);
-    data->philo = init_philo(argv);
-    if (data->philo == NULL)
-    {
-        printf("Error during allocation\n");
-        pthread_mutex_destroy(&data->left_fork);
-        pthread_mutex_destroy(&data->right_fork);
-        free(data);
-        return (0);
-    }
-	return (data);
+    return (data);
 }
