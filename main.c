6ee3d3e28ba848d079f81dde8474c45300b270c2
emmameinert <110816436+emmameinert@users.noreply.github.com>
2023-06-07 16:48:09 +0300

    Add files via upload
    main evil updated

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
 #include "philo.h"
 
+int death_check(t_philo **philo)
+{
+    int i;
+
+    i = 0;
+    while (i < (*philo)->data->philo_nb)
+    {
+        pthread_mutex_lock(&((*philo)->data->death_lock));
+        if ((philo[i]->last_meal > 0) && (time_diff(philo[i]->last_meal) > philo[i]->time_to_die)) //vor dem ersten meal logischerweise falsche zeit, weil es noch kein meal gab. somti loest da aus und die philos sterben sofort
+        {
+            print_message(((*philo)->data), time_diff((*philo)->data->start_time), (philo[i])->id, 'd');
+            pthread_mutex_unlock(&((*philo)->data->death_lock));
+            return (1);
+        }
+        pthread_mutex_unlock(&((*philo)->data->death_lock));
+        i++; 
+    //maybe add the eating count here
+    }
+    return (0);
+}
+
 int main(int argc, char **argv)
 {
     t_philo **philo;
@@ -17,11 +38,9 @@ int main(int argc, char **argv)
     if ((*philo) == NULL)
         return (0);
     parsing((*philo)->data, argv, philo);
-    //printf("%lld\n",(*philo)->data->philo_nb);
     start_routine(philo);
     pthread_mutex_destroy(&(*philo)->data->eating_lock);
-    pthread_mutex_destroy(&(*philo)->data->left_fork);
-    pthread_mutex_destroy(&(*philo)->data->right_fork);
-    free((*philo)->data);
+    free((*philo)->data); //ist das richtig so?
+    free_struct(philo, (*philo)->data->philo_nb);
     return (0);
 }
