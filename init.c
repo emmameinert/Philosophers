b7eedff76029b08e36ec6c7316d6e3028eb44f34
emmameinert <110816436+emmameinert@users.noreply.github.com>
2023-06-10 21:03:18 +0300

    Add files via upload
    actually new init

--- init.c
+++ init.c
@@ -11,6 +11,8 @@ t_philo **init_philo(char **argv, t_data *data)
     philo = malloc(sizeof(t_philo) * philosophers); 
     if (!philo)
         return (NULL);
+    //data->fork = malloc(sizeof(pthread_mutex_t) * (data->philo_nb)); muss mal spaeter schauen ob das her besser waere als im parsing
+    
     while (i < philosophers)
     {
         philo[i] = malloc(sizeof(t_philo) * 1);
@@ -21,6 +23,8 @@ t_philo **init_philo(char **argv, t_data *data)
         }
         philo[i]->data = data;
         pthread_mutex_init(&((philo[i])->eating_mutex), NULL);
+        //pthread_mutex_init(&(philo[i]->data->fork), NULL);
+        //pthread_mutex_init(&(data->fork[i]), NULL);
         i++;
     }
     return (philo);
@@ -45,11 +49,10 @@ t_data *init_all(void)
     data = init_data();
     if (data == NULL)   
         return (NULL);
-    pthread_mutex_init(&(data->eating_lock), NULL);
     pthread_mutex_init(&(data->printing), NULL);
     pthread_mutex_init(&(data->eating_count_lock), NULL);
     pthread_mutex_init(&(data->death_lock), NULL);
+    pthread_mutex_init(&(data->routine_lock), NULL);
     data->start_time = get_time();
-    //printf("start time : %lld\n", time_diff(data));
     return (data);
 }
