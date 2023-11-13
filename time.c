689f3b935a4656e72a10dd237df09b501db18a99
emmameinert <110816436+emmameinert@users.noreply.github.com>
2023-11-13 12:45:06 +0200

    Add files via upload

--- time.c
+++ time.c
@@ -1,22 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emeinert <emeinert@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/21 19:09:55 by emeinert          #+#    #+#             */
+/*   Updated: 2023/06/21 21:12:50 by emeinert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 
-unsigned long long get_time()
+long long	get_time(void)
 {
-    struct timeval  tv;
-    unsigned long long  ms;
+	struct timeval		tv;
+	unsigned long long	ms;
 
-    ms = 0;
-    if (gettimeofday(&tv, NULL) == 0)
-        ms = (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
-    else 
-        ms = 0;
-    return (ms);
+	ms = 0;
+	if (gettimeofday(&tv, NULL) == 0)
+		ms = (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
+	else
+		ms = 0;
+	return (ms);
 }
 
-long long   time_diff(unsigned long long pres, unsigned long long past)
+long long	time_diff(long long pres, long long past)
 {
-    long long diff;
+	long long	diff;
 
-    diff = pres - past;
-    return (diff);
+	diff = pres - past;
+	return (diff);
 }
